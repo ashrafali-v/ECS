0b5a8a214e58b85bedc6e459294a2a610f677f2d
amaljithUid <amaljith82@gmail.com>
2020-06-23 21:50:37 +0530

    "Ui fixes bill comparison & neighbour hood"

--- src/app/neighbour-comparison/neighbour-comparison.component.ts
+++ src/app/neighbour-comparison/neighbour-comparison.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
-import {CommonAppService} from '../services/common-app.service';
+import { CommonAppService } from '../services/common-app.service';
 @Component({
   selector: 'app-neighbour-comparison',
   templateUrl: './neighbour-comparison.component.html',
@@ -13,46 +13,51 @@ export class NeighbourComparisonComponent implements OnInit {
   amountValue: number;
   kilowatsValue: number;
   multiAmount: any[];
-  multiKwh:any[];
-  lineChartDataAmount:any[];
-  lineChartDataKwh:any[];
+  multiKwh: any[];
+  lineChartDataAmount: any[];
+  lineChartDataKwh: any[];
   view: any[];
   /*-----------------Line chart config-----------------------------*/
   public lineChartData: ChartDataSets[];
   public lineChartLabels: Label[];
   public lineChartOptions: ChartOptions = {
     responsive: true,
-    legend: { 
-      position: 'top',
+    legend: {
+      position: 'bottom',
+      display: false,
       labels: {
-        fontSize: 7,
+        fontSize: 12,
+        fontFamily: 'Karla',
         //usePointStyle: true,
-        boxWidth:30
+        boxWidth: 30,
+
       }
     },
     animation: {
-      duration:2000
+      duration: 2000
     },
     layout: {
       padding: {
-          left: 10,
-          right: 0,
-          top: 0,
-          bottom: 0
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
       }
     },
     scales: {
       yAxes: [{
-          ticks: {
-              // Include a dollar sign in the ticks
-              callback: function(value, index, values) {
-                  return '$' + value;
-              },
-              fontSize:10
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return '$' + value;
           },
-          gridLines: {
-            display: false,
-          }
+          fontSize: 16,
+          fontFamily: 'Karla',
+        },
+        gridLines: {
+          display: true,
+          borderDash: [8, 4]
+        }
       }],
       xAxes: [{
         display: false, //this will remove all the x-axis grid lines
@@ -81,66 +86,66 @@ export class NeighbourComparisonComponent implements OnInit {
   /*-----------------Bar chart config-----------------------------*/
 
   colorScheme = {
-    domain: ['#fd7e14', 'black', 'blue','blue']
+    domain: ['#fd7e14', 'black', 'blue', 'blue']
   };
 
   /*-----------------Bar chart config-----------------------------*/
   constructor(private sharedService: CommonAppService) { }
 
   ngOnInit(): void {
-  this.sharedService.nextMessage("amount");
-  /*-----------------Bar chart config-----------------------------*/
-  this.view = [300,200];
-  this.multiAmount = [
-    {
-      "name": "All Neighbours",
-      "value": 215
-    },
-    {
-      "name": "Similar Homes",
-      "value": 180
-    },
-    {
-      "name": "Your Usage",
-      "value": 198
-    },
-    {
-      "name": "Your Current Month",
-      "value": 63
-    }
-  ]
-  this.multiKwh = [
-    {
-      "name": "All Neighbours",
-      "value": 205
-    },
-    {
-      "name": "Similar Homes",
-      "value": 160
-    },
-    {
-      "name": "Your Usage",
-      "value": 188
-    },
-    {
-      "name": "Your Current Month",
-      "value": 83
-    }
-  ]
-  Object.assign(this.multiAmount);
-  Object.assign(this.multiKwh)
-  /*-----------------Bar chart config End-----------------------------*/
-    this.lineChartData =[];
+    this.sharedService.nextMessage("amount");
+    /*-----------------Bar chart config-----------------------------*/
+    this.view = [320, 200];
+    this.multiAmount = [
+      {
+        "name": "All Neighbours",
+        "value": 215
+      },
+      {
+        "name": "Similar Homes",
+        "value": 180
+      },
+      {
+        "name": "Your Usage",
+        "value": 198
+      },
+      {
+        "name": "Your Current Month",
+        "value": 63
+      }
+    ]
+    this.multiKwh = [
+      {
+        "name": "All Neighbours",
+        "value": 205
+      },
+      {
+        "name": "Similar Homes",
+        "value": 160
+      },
+      {
+        "name": "Your Usage",
+        "value": 188
+      },
+      {
+        "name": "Your Current Month",
+        "value": 83
+      }
+    ]
+    Object.assign(this.multiAmount);
+    Object.assign(this.multiKwh)
+    /*-----------------Bar chart config End-----------------------------*/
+    this.lineChartData = [];
     this.amountValue = 128;
     this.kilowatsValue = 185;
-    this.lineChartLabels = ['1','2','3','4','5','6','7','8','9','10'];
-    this.lineChartDataAmount = [{ data: [0, 200, 130, 210, 216, 250, 200], label: 'All Neighbours',fill: false },
-    { data: [0, 75, 125, 190, 200, 115, 130], label: 'Similar Homes',fill: false },
-    { data: [0, 150, 110, 170, 240, 225, 150], label: 'Your Usage',fill: false }];
+    this.lineChartLabels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    this.lineChartDataAmount = [{ data: [0, 200, 130, 210, 216, 250, 200], label: 'All Neighbours', fill: false },
+    { data: [0, 75, 125, 190, 200, 115, 130], label: 'Similar Homes', fill: false },
+    { data: [0, 150, 110, 170, 240, 225, 150], label: 'Your Usage', fill: false }];
 
-    this.lineChartDataKwh = [{ data: [0, 100, 130, 250, 116, 150, 210], label: 'All Neighbours',fill: false },
-    { data: [0, 175, 145, 180, 250, 215, 170], label: 'Similar Homes',fill: false },
-    { data: [0, 130, 210, 170, 200, 225, 150], label: 'Your Usage',fill: false }];
+    this.lineChartDataKwh = [{ data: [0, 100, 130, 250, 116, 150, 210], label: 'All Neighbours', fill: false },
+    { data: [0, 175, 145, 180, 250, 215, 170], label: 'Similar Homes', fill: false },
+    { data: [0, 130, 210, 170, 200, 225, 150], label: 'Your Usage', fill: false }];
 
     this.lineChartData = this.lineChartDataAmount;
 
