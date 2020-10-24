7fcced7dc48e305cd53ea543615df5610dde291c
ashrafali-v <ashruashrafali@gmail.com>
2020-10-24 23:37:22 +0530

    usage report basic structure.

--- src/app/usage-report/usage-report.component.ts
+++ src/app/usage-report/usage-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-usage-report',
@@ -6,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usage-report.component.scss']
 })
 export class UsageReportComponent implements OnInit {
-
+    /*------------Dummy-------------*/
+    series = [
+      {
+        "name": "Computer",
+        "value": 20,
+        "label": "20%"
+      },
+      {
+        "name": "Washer & Dryer",
+        "value": 70,
+        "label": "70%"
+      },
+      {
+        "name": "Dishwasher",
+        "value": 10,
+        "label": "10%"
+      },
+      {
+        "name": "Lighting",
+        "value": 30,
+        "label": "70%"
+      },
+      {
+        "name": "Electric oven",
+        "value": 90,
+        "label": "10%"
+      },{
+        "name": "Cooling",
+        "value": 45,
+        "label": "70%"
+      }
+    ];
+    /*-------------------------*/
   constructor() { }
 
   ngOnInit(): void {
